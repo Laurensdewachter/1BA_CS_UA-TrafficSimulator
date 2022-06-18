e1b0043e72bae4cd43223446bede19540bad13bf
laurensdewachter <laurens.dewachter@hotmail.com>
2022-06-18 11:05:50 +0200

    FireEngine

--- objects/vehicles/FireEngine.h
+++ objects/vehicles/FireEngine.h
@@ -17,26 +17,65 @@ const double fireEngineMaxBrakeFactor = 4.56;
 const double fireEngineMinFollowDistance = 10.0;
 
 class FireEngine : public Vehicle{
+    FireEngine* _initCheck;
+
 public:
+    /*
+     * ENSURE(properlyInitialized(), "FireEngine constructor did not end in an initialized state")
+     */
     FireEngine(const std::string &street, double position);
 
     ~FireEngine();
 
+    bool properlyInitialized() const;
+
+    /*
+     * REQUIRE(properlyInitialized(), "FireEngine wasn't initialized when calling getLength()")
+     */
     double getLength() const;
 
+    /*
+     * REQUIRE(properlyInitialized(), "FireEngine wasn't initialized when calling getType()")
+     */
     std::string getType() const;
 
+    /*
+     * REQUIRE(properlyInitialized(), "FireEngine wasn't initialized when calling getAcronym()")
+     */
     char getAcronym() const;
 
+    /*
+     * REQUIRE(properlyInitialized(), "FireEngine wasn't initialized when calling drive()")
+     *
+     * ENSURE(fPosition >= startPosition || std::abs(fPosition-startPosition) < 0.001, "drive() postcondition")
+     */
     void drive(Vehicle* vehicleInFront);
 
+    /*
+     * REQUIRE(properlyInitialized(), "FireEngine wasn't initialized when calling brake()")
+     *
+     * ENSURE(fMaxSpeed <= fireEngineMaxSpeed, "brake() postcondition");
+     * ENSURE(fMaxSpeed <= startMaxSpeed, "brake() postcondition");
+     */
     void brake();
 
+    /*
+     * REQUIRE(properlyInitialized(), "FireEngine wasn't initialized when calling stop()")
+     *
+     * ENSURE(fAcceleration <= startAcceleration, "stop() postcondition")
+     */
     void stop();
 
+    /*
+     * REQUIRE(properlyInitialized(), "FireEngine wasn't initialized when calling setMaxSpeed()")
+     */
     void setMaxSpeed();
 
+    /*
+     * REQUIRE(properlyInitialized(), "FireEngine wasn't initialized when calling hasPriority()")
+     */
     bool hasPriority() const;
 };
+
 
 #endif
