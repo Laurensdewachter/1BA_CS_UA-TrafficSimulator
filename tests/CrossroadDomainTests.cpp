f53972f194845d3bc98659c43c98a2a67da766bd
laurensdewachter <laurens.dewachter@hotmail.com>
2022-06-21 13:47:39 +0200

    template crossroads tests

---
+++ tests/CrossroadDomainTests.cpp
@@ -0,0 +1,26 @@
+// ===========================================================
+// Name         : CrossroadDomainTests.cpp
+// Author       : Laurens De Wachter & Nabil El Ouaamari
+// Version      : 1.0
+// Description  : This code tests the `Crossraod` class of the TrafficSimulation.
+// ===========================================================
+
+#include "gtest/gtest.h"
+#include "../objects/Crossroad.h"
+
+class CrossroadDomainTest : public ::testing::Test {
+protected:
+    virtual void SetUp() {}
+
+    virtual void TearDown() {}
+
+    Crossroad crossroad;
+};
+
+TEST_F(CrossroadDomainTest, Constructor) {
+
+}
+
+TEST_F(CrossroadDomainTest, HappyDay) {
+
+}
